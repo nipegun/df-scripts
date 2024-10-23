1c25ef9147cfb52b9c41869b4d017f8f4bf0cee0
NiPeGun <nipegun@gmail.com>
2024-10-23 22:39:45 +0200

    Update RegRipper-MrEvil.sh

--- RegRipper-MrEvil.sh
+++ RegRipper-MrEvil.sh
@@ -9,9 +9,6 @@
   rm -rf $vCarpetaDeCasos$vCasoActual 2>/dev/null
   sudo mkdir -p $vCarpetaDeCasos$vCasoActual/Registro/
 
-# Reparar permisos
-  sudo chown 1000:1000 $vCarpetaDeCasos -R
-
 # Copiar archivos de registro
   echo ""
   echo "  Copiando SYSTEM..."
@@ -34,7 +31,7 @@
   echo ""
   sudo cp "$vPuntoDeMontaje"/WINDOWS/system32/config/default  "$vCarpetaDeCasos""$vCasoActual"/Registro/DEFAULT
 
-# Exportando registro de usuarios
+# Copiar registro de usuarios
   echo ""
   echo "  Copiando archivos de registro de usuarios..."
   echo ""
@@ -48,27 +45,44 @@
     cp "$vPuntoDeMontaje"/"Documents and Settings"/"$vNomUsuario"/NTUSER.DAT "$vCarpetaDeCasos$vCasoActual"/Registro/Usuarios/"$vNomUsuario"/
   done < "/tmp/CarpetasDeUsuarios.txt"
 
-
 # Exportar registros
   sudo mkdir -p "$vCarpetaDeCasos""$vCasoActual"/RegRipper/ 2> /dev/null
   echo ""
-  echo "  Exportando SYSTEM..."
+  echo "  RegRippeando SYSTEM..."
   echo ""
   sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos""$vCasoActual"/Registro/SYSTEM   -a > "$vCarpetaDeCasos""$vCasoActual"/RegRipper/SYSTEM.txt
   echo ""
-  echo "  Exportando SAM..."
+  echo "  RegRippeando SAM..."
   echo ""
   sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos""$vCasoActual"/Registro/SAM      -a > "$vCarpetaDeCasos""$vCasoActual"/RegRipper/SAM.txt
   echo ""
-  echo "  Exportando SECURITY..."
+  echo "  RegRippeando SECURITY..."
   echo ""
   sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos""$vCasoActual"/Registro/SECURITY -a > "$vCarpetaDeCasos""$vCasoActual"/RegRipper/SECURITY.txt
   echo ""
-  echo "  Exportando SOFTWARE..."
+  echo "  RegRippeando SOFTWARE..."
   echo ""
   sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos""$vCasoActual"/Registro/SOFTWARE -a > "$vCarpetaDeCasos""$vCasoActual"/RegRipper/SOFTWARE.txt
   echo ""
-  echo "  Exportando DEFAULT..."
+  echo "  RegRippeando DEFAULT..."
   echo ""
   sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos""$vCasoActual"/Registro/DEFAULT  -a > "$vCarpetaDeCasos""$vCasoActual"/RegRipper/DEFAULT.txt
+
+# Exportar registro de usuarios
+  echo ""
+  echo "  RegRippeando archivos de registro de usuarios..."
+  echo ""
+  find "$vPuntoDeMontaje/Documents and Settings/" -mindepth 1 -maxdepth 1 -type d > /tmp/CarpetasDeUsuarios.txt
+  while IFS= read -r linea; do
+    vNomUsuario="${linea##*/}"
+    echo ""
+    echo "    RegRippeando NTUSER.DAT de $vNomUsuario..."
+    echo ""
+    sudo mkdir -p "$vCarpetaDeCasos$vCasoActual"/RegRipper/Usuarios/"$vNomUsuario" 2> /dev/null
+    sudo /usr/local/bin/rip.pl -r "$vCarpetaDeCasos$vCasoActual"/Registro/Usuarios/"$vNomUsuario"/NTUSER.DAT  -a > "$vCarpetaDeCasos$vCasoActual"/Registro/Usuarios/"$vNomUsuario"/NTUSER.DAT.txt
+  done < "/tmp/CarpetasDeUsuarios.txt"
+
+# Reparar permisos
+  sudo chown 1000:1000 $vCarpetaDeCasos -R
+
 
