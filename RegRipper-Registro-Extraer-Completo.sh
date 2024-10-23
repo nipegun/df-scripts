b9b4196501318d6a162456fad6dbf1ce17c4ac39
NiPeGun <nipegun@gmail.com>
2024-10-23 12:13:33 +0200

    Update RegRipper-Registro-Extraer-Completo.sh

--- RegRipper-Registro-Extraer-Completo.sh
+++ RegRipper-Registro-Extraer-Completo.sh
@@ -33,17 +33,6 @@ vPuntoDeMontaje="/Particiones/Pruebas"
 
 
 
-vPuntoDeMontaje="/Particiones/Pruebas"
-vCarpetaDelCaso="/Casos/MrEvil"
-sudo mkdir -p $vCarpetaDelCaso
-sudo chown 1000:1000 $vCarpetaDelCaso -R
-sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/system   -a > $vCarpetaDelCaso/SYSTEM.txt
-sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SAM      -a > $vCarpetaDelCaso/SAM.txt
-sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SECURITY -a > $vCarpetaDelCaso/SECURITY.txt
-sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/software -a > $vCarpetaDelCaso/SOFTWARE.txt
-sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/default  -a > $vCarpetaDelCaso/DEFAULT.txt
-
-
 #
   /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SYSTEM   -a > /Particiones/SYSTEM.txt
 # 
@@ -55,3 +44,31 @@ sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/default  
 # 
   /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/DEFAULT  -a > /Particiones/DEFAULT.txt
 
+
+
+
+
+
+
+# ------
+  vPuntoDeMontaje="/Particiones/Pruebas"
+  vCarpetaDeCasos="/Casos"
+  # Determinar el caso actual
+  vCasoActual="/22"
+  sudo mkdir -p $vCarpetaDeCasos$vCasoActual
+  sudo chown 1000:1000 $vCarpetaDeCasos -R
+  sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/system   -a > $vCarpetaDeCasos$vCasoActual/SYSTEM.txt
+  sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SAM      -a > $vCarpetaDeCasos$vCasoActual/SAM.txt
+  sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SECURITY -a > $vCarpetaDeCasos$vCasoActual/SECURITY.txt
+  sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/software -a > $vCarpetaDeCasos$vCasoActual/SOFTWARE.txt
+  sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/default  -a > $vCarpetaDeCasos$vCasoActual/DEFAULT.txt
+
+vPuntoDeMontaje="/Particiones/Pruebas"
+for vCarpeta in "$vPuntoDeMontaje/Documents and Settings/*"; do
+  #if [ -d "$vCarpeta" ]; then
+    # Comandos a ejecutar por cada carpeta
+    echo "Procesando carpeta: $vCarpeta"
+    # Tus comandos aquí, por ejemplo:
+    # cd "$dir" && ejecutar_comando
+  #fi
+done
