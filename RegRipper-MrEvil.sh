88d14ff58706dbc79f49e6631cb69337db820a45
NiPeGun <nipegun@gmail.com>
2024-10-23 20:04:05 +0200

    Update RegRipper-MrEvil.sh

--- RegRipper-MrEvil.sh
+++ RegRipper-MrEvil.sh
@@ -7,10 +7,25 @@
   vCasoActual="/22"
   sudo mkdir -p $vCarpetaDeCasos$vCasoActual
   sudo chown 1000:1000 $vCarpetaDeCasos -R
+  echo ""
+  echo "  Exportando SYSTEM..."
+  echo ""
   sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/system   -a > $vCarpetaDeCasos$vCasoActual/SYSTEM.txt
+  echo ""
+  echo "  Exportando SAM..."
+  echo ""
   sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SAM      -a > $vCarpetaDeCasos$vCasoActual/SAM.txt
+  echo ""
+  echo "  Exportando SECURITY..."
+  echo ""
   sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/SECURITY -a > $vCarpetaDeCasos$vCasoActual/SECURITY.txt
+  echo ""
+  echo "  Exportando SOFTWARE..."
+  echo ""
   sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/software -a > $vCarpetaDeCasos$vCasoActual/SOFTWARE.txt
+  echo ""
+  echo "  Exportando DEFAULT..."
+  echo ""
   sudo /usr/local/bin/rip.pl -r $vPuntoDeMontaje/WINDOWS/system32/config/default  -a > $vCarpetaDeCasos$vCasoActual/DEFAULT.txt
 
 vPuntoDeMontaje="/Particiones/Pruebas"
