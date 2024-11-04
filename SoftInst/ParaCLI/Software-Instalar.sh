44030650e42a8d3f3c8f0dbbcfd60dc7bced0b97
NiPeGun <nipegun@gmail.com>
2024-11-04 09:04:30 +0100

    Update Software-Instalar.sh

--- SoftInst/ParaCLI/Software-Instalar.sh
+++ SoftInst/ParaCLI/Software-Instalar.sh
@@ -1,2 +1,8 @@
+# 
+  apt-get -y update
+
 # Análisis de archivos y discos (Contiene la herramienta mmls y otras)
   apt-get -y install sleuthkit
+
+# Herramientas para el registro de Windows
+  apt-get -y install chntpw
