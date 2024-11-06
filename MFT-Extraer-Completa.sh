e271435e8d472dee99072bc75eb1ac975c094856
NiPeGun <nipegun@gmail.com>
2024-11-06 10:28:25 +0100

    Update MFT-Extraer-Completa.sh

--- MFT-Extraer-Completa.sh
+++ MFT-Extraer-Completa.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantParamEsperados=1
+
+if [ $# -ne $cCantParamEsperados ]
+  then
+    echo ""
+    echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+    echo "    $0 [PuntoDeMontajeDeLaPartNTFS]"
+    echo ""
+    echo "  Ejemplo:"
+    echo "    $0 '/mnt/DiscoWindows/'"
+    echo ""
+    exit
+  else
+    echo ""
+    echo ""
+    echo ""
+    cat $1$2
+fi
