b8049c1456983f40eac95f7cbb185ae1810d8d46
NiPeGun <nipegun@gmail.com>
2024-11-09 23:53:06 +0100

    Update and rename Plaso-Instalar1.sh to Plaso-Instalar2.sh

--- SoftInst/ParaCLI/Plaso-Instalar2.sh
+++ SoftInst/ParaCLI/Plaso-Instalar2.sh
@@ -10,8 +10,11 @@
   apt -y install liblzma-dev
 
 # Instalar plaso
-  rm -rf /root/SoftInst/Plaso/*
+  echo ""
+  echo "  Creando la carpeta para guardar el código fuente..."
+  echo ""
   mkdir -p /root/SoftInst/Plaso/
+  rm -rf /root/SoftInst/Plaso/*
   cd /root/SoftInst/Plaso/
     # Comprobar si el paquete python3-venv está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s python3-venv 2>/dev/null | grep installed) == "" ]]; then
@@ -21,7 +24,13 @@
       apt-get -y update && apt-get -y install python3-venv
       echo ""
     fi
+  echo ""
+  echo "  Creando el ambiente virtual..."
+  echo ""
   python3 -m venv plaso
+  echo ""
+  echo "  Entrando al ambiente virtual"
+  echo ""
   source plaso/bin/activate
   # Comprobar si el paquete python3-pip está instalado. Si no lo está, instalarlo.
     if [[ $(dpkg-query -s python3-pip 2>/dev/null | grep installed) == "" ]]; then
@@ -31,10 +40,16 @@
       apt-get -y update && apt-get -y install python3-pip
       echo ""
     fi
+  echo ""
+  echo "  Instalando plaso..."
+  echo ""
   pip3 install plaso
   # Verificando que log2timeline se haya descargado
     ./plaso/bin/log2timeline --version
-  # Compilar el script
+  # Compilar
+    echo ""
+    echo "  Compilando log2timeline..."
+    echo ""
     pip install pyinstaller
     pyinstaller --onefile /root/SoftInst/Plaso/plaso/bin/log2timeline
     pyinstaller --onefile \
