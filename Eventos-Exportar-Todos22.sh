cdbbbd844b29403f3b90e6e424ce4f7644cd35f4
NiPeGun <nipegun@gmail.com>
2024-11-10 18:45:09 +0100

    Update and rename Eventos-Exportar-Todos21.sh to Eventos-Exportar-Todos22.sh

--- Eventos-Exportar-Todos22.sh
+++ Eventos-Exportar-Todos22.sh
@@ -51,11 +51,17 @@ if [ $# -ne $cCantParamEsperados ]
     vCarpetaDelCaso=$2
 
     # Copiar los eventos crudos
+      echo ""
+      echo "  Copiando todos los eventos .evtx de la partición de Windows a la carpeta del caso..."
+      echo ""
       mkdir -p $vCarpetaDelCaso/Eventos/Crudos/
       rm -rf $vCarpetaDelCaso/Eventos/Crudos/*
       find $vPuntoDeMontajePartWindows -name "*.evtx" -exec cp {} $vCarpetaDelCaso/Eventos/Crudos/ \;
 
     # Convertir los eventos a xml
+      echo ""
+      echo "  Exportando cada archivo .evtx a .xml..."
+      echo ""
       # Comprobar si el paquete libevtx-utils está instalado. Si no lo está, instalarlo.
         if [[ $(dpkg-query -s libevtx-utils 2>/dev/null | grep installed) == "" ]]; then
           echo ""
@@ -73,8 +79,8 @@ if [ $# -ne $cCantParamEsperados ]
         find $vCarpetaDelCaso/Eventos/Crudos/ -name "*.evtx" | while read vArchivo; do
           vArchivoDeSalida="$vCarpetaDelCaso/Eventos/Parseados/XML/$(basename "$vArchivo" .evtx).xml"
           evtxexport -f xml "$vArchivo" > "$vArchivoDeSalida" && sed -i '1d' "$vArchivoDeSalida" && sed -i 's/^<Event [^>]*>/<Event>/' "$vArchivoDeSalida"
-          sed -i '1i\<root>' "$vArchivoDeSalida"
-          echo '</root>' >> "$vArchivoDeSalida"
+          #sed -i '1i\<root>' "$vArchivoDeSalida"
+          #echo '</root>' >> "$vArchivoDeSalida"
         done
         # Borrar todos los xml que no tengan la linea <Event>
           for archivo in "$vCarpetaDelCaso/Eventos/Parseados/XML"/*; do # Recorre todos los archivos en el directorio
@@ -82,20 +88,23 @@ if [ $# -ne $cCantParamEsperados ]
               rm -f "$archivo" # Si no contiene "<Event>", lo elimina
             fi
           done
-        # Pasar todos los eventos xml a un único archivo XML
-        #  echo ""
-        #  echo "  Unificando todos los archivos .xml en un único archivo..."
-        #  echo ""
-        #  for archivo in "$vCarpetaDelCaso/Eventos/Parseados/XML"/*; do # Recorre todos los archivos en el directorio
-        #    cat "$archivo" >> "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventos.xml
-        #  done
-        # Agregar una etiqueta raíz para poder trabajar con los xml
-        #  for archivo in "$vCarpetaDelCaso/Eventos/Parseados/XML"/*; do # Recorre todos los archivos en el directorio
-        #    echo "<root>" > nuevo_archivo.xml
-        #    cat archivo.xml >> nuevo_archivo.xml
-        #    echo "</root>" >> nuevo_archivo.xml
-        #  done
 
+      # Pasar todos los eventos xml a un único archivo XML
+        echo ""
+        echo "  Unificando todos los archivos .xml en un único archivo..."
+        echo ""
+        for archivo in "$vCarpetaDelCaso/Eventos/Parseados/XML"/*; do # Recorre todos los archivos en el directorio
+          cat "$archivo" >> "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventos.xml
+        done
+        # Agregar una etiqueta raíz para poder trabajar con el xml
+          sed -i '1i\<root>' "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventos.xml # Agrega la apertura de la etiqueta raiz en la primera linea
+          echo '</root>' >>  "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventos.xml # Agrega el cierre de la etiqueta raíz en una nueva linea al final del archivo
+
+      # Agregar una etiqueta raíz para poder trabajar con los xml
+        for vArchivo in "$vCarpetaDelCaso/Eventos/Parseados/XML"/*; do # Recorre todos los archivos en el directorio
+          sed -i '1i\<root>' "$vArchivo"                               # Agrega la apertura de la etiqueta raiz en la primera linea
+          echo '</root>' >> "$vArchivo"                                # Agrega el cierre de la etiqueta raíz en una nueva linea al final del archivo
+        done
 
       # También convertir a texto
         mkdir -p $vCarpetaDelCaso/Eventos/Parseados/TXT/
@@ -109,29 +118,35 @@ if [ $# -ne $cCantParamEsperados ]
         done
 
     # Convertir los eventos a log2timeline
-      ~/SoftInst/Plaso/plaso/bin/log2timeline $vCarpetaDelCaso/Eventos/Crudos/ --storage-file $vCarpetaDelCaso/Eventos/Parseados/TimeLine.plaso
+      #~/SoftInst/Plaso/plaso/bin/log2timeline $vCarpetaDelCaso/Eventos/Crudos/ --storage-file $vCarpetaDelCaso/Eventos/Parseados/TimeLine.plaso
     # Parsear
-      ~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o dynamic   -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.txt
-      ~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o json      -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.json
+      #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o dynamic   -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.txt
+      #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o json      -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.json
       #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o json_line -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.json_line 
       #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o l2tcsv    -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.l2tcsv
       #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o l2ttln    -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.l2ttln
       #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o rawpy     -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.rawpy
       #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o tln       -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.tln
-      ~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o xlsx      -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.xlsx
+      #~/SoftInst/Plaso/plaso/bin/psort "$vCarpetaDelCaso"/Eventos/Parseados/TimeLine.plaso -o xlsx      -w "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.xlsx
 
      # Pasar todo el TimeLine de eventos, de json a xml
-       cat "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.json | jq | grep xml_string | sed 's-"xml_string": "--g' | sed 's/\\n/\n/g' | sed '/^"/d' | sed 's-xmlns=\"http://schemas.microsoft.com/win/2004/08/events/event\"--g' > "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineCompleto.xml
+      # cat "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineEventos.json | jq | grep xml_string | sed 's-"xml_string": "--g' | sed 's/\\n/\n/g' | sed '/^"/d' | sed 's-xmlns=\"http://schemas.microsoft.com/win/2004/08/events/event\"--g' > "$vCarpetaDelCaso"/Eventos/Parseados/TimeLineCompleto.xml
+
+    # Eventos sólo del usuario
+      vSIDDelUsuario="S-1-5-21-92896240-835188504-1963242017-1001"
+      xmllint --xpath '//*[Data[@Name="SubjectUserSid" and text()='"'$vSIDDelUsuario'"']]/parent::*' /Casos/Examen/Eventos/Parseados/XML/*  > $vCarpetaDelCaso/Eventos/Parseados/EventosDelUsuario.xml 2> /dev/null
+      xmllint --xpath '//*[Security[@UserID='"'$vSIDDelUsuario'"']]/parent::*'                       /Casos/Examen/Eventos/Parseados/XML/* >> $vCarpetaDelCaso/Eventos/Parseados/EventosDelUsuario.xml 2> /dev/null
+
 
 
      # Exportando actividad del usuario específico desde el archivo .json
-       echo ""
-       echo "  Exportando actividad específica del usuario ..."
-       echo ""
-       vSIDDelUsuario="S-1-5-21-92896240-835188504-1963242017-1001"
-       cat '/Casos/Examen/Eventos/Parseados/TimeLineEventos.json' | sed 's-/Casos/Examen/Eventos/Crudos/--g'  | jq '.[] | select(.user_sid == "'"$vSIDDelUsuario"'")' > $vCarpetaDelCaso/Eventos/Parseados/TimeLineUsuario.json
+     #  echo ""
+     #  echo "  Exportando actividad específica del usuario ..."
+     #  echo ""
+     #  vSIDDelUsuario="S-1-5-21-92896240-835188504-1963242017-1001"
+     #  cat '/Casos/Examen/Eventos/Parseados/TimeLineEventos.json' | sed 's-/Casos/Examen/Eventos/Crudos/--g'  | jq '.[] | select(.user_sid == "'"$vSIDDelUsuario"'")' > $vCarpetaDelCaso/Eventos/Parseados/TimeLineUsuario.json
 
-     cat /Casos/Examen/Eventos/Parseados/TimeLineEventos.txt | sed 's-/Casos/Examen/Eventos/Crudos/--g' | grep S-1-5-21 > $vCarpetaDelCaso/Eventos/Parseados/TimeLineUsuario.txt
+#     cat /Casos/Examen/Eventos/Parseados/TimeLineEventos.txt | sed 's-/Casos/Examen/Eventos/Crudos/--g' | grep S-1-5-21 > $vCarpetaDelCaso/Eventos/Parseados/TimeLineUsuario.txt
 
 
 fi
