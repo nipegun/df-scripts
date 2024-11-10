1ca652918e50906a8e6198a873fcdc30601ad880
NiPeGun <nipegun@gmail.com>
2024-11-10 19:54:40 +0100

    Update and rename Eventos-Exportar-Todos26.sh to Eventos-Exportar-Todos27.sh

--- Eventos-Exportar-Todos27.sh
+++ Eventos-Exportar-Todos27.sh
@@ -146,7 +146,40 @@ if [ $# -ne $cCantParamEsperados ]
       xmllint --xpath '//*[Security[@UserID='"'$vSIDDelUsuario'"']]/parent::*'                       /Casos/Examen/Eventos/Parseados/XML/* >> "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventosDelUsuario.xml 2> /dev/null
       sed -i '1i\<root>' "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventosDelUsuario.xml # Agrega la apertura de la etiqueta raiz en la primera linea
       echo '</root>' >>  "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventosDelUsuario.xml # Agrega el cierre de la etiqueta raíz en una nueva linea al final del archivo
-          
+      # Generar un archivo por cada evento dentro del xml
+        mkdir -p "$vCarpetaDelCaso"/Eventos/Parseados/XML/EventosIndividualesDeUsuario/
+        vcontadorDeEventos=1
+        # Variable para almacenar un evento temporalmente
+          vEvento=""
+        # Leer el archivo línea por línea
+          while IFS= read -r line; do
+            if [[ "$line" == *"<Event>"* ]]; then
+              vEvento="$line"# Iniciar un nuevo bloque de evento
+            elif [[ "$line" == *"</Event>"* ]]; then
+              vEvento+=$'\n'"$line" # Agregar la línea de cierre del evento
+              # Guardar el bloque en un archivo
+                output_file="eventos_separados/evento_${contador}.xml"
+                echo "$evento" > "$output_file"
+              # Incrementar el contador y limpiar la variable del evento
+                vContadorDeEventos=$((vContadorDeEventos + 1))
+                vEvento=""
+            else
+              vEvento+=$'\n'"$line" # Agregar la línea al bloque de evento en curso
+            fi
+          done < "$vCarpetaDelCaso"/Eventos/Parseados/TodosLosEventosDelUsuario.xml
+        # Renombrar cada archivo con el valor del campo SystemTime
+          eventos_folder="EventosOrdenadorPorFecha"
+          # Recorrer cada archivo XML en la carpeta
+            for file in "$vCarpetaDelCaso"/Eventos/Parseados/XML/EventosIndividualesDeUsuario/*.xml; do
+              # Extraer el valor de SystemTime usando xmlstarlet
+                system_time=$(xmlstarlet sel -t -v "//TimeCreated/@SystemTime" "$file" 2>/dev/null)
+              # Renombrar el archivo
+                new_filename="${eventos_folder}/${system_time}.xml"
+                mv "$file" "$new_filename"
+            done
+
+
+
      # Exportando actividad del usuario específico desde el archivo .json
      #  echo ""
      #  echo "  Exportando actividad específica del usuario ..."
