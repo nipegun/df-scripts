6f6e8d208b40bf7c0b2167a35e7e4d7c0aceb271
NiPeGun <nipegun@gmail.com>
2024-11-15 11:28:59 +0100

    Create Imagen-Particiones-Todas-Desmontar.sh

---
+++ Imagen-Particiones-Todas-Desmontar.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para montar todas las particiones de dentro de un archivo de imagen
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Imagen-Particiones-Montar-SoloLectura.sh | sudo bash -s [RutaAlArchivoDeImagen]
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Imagen-Particiones-Montar-SoloLectura.sh | nano -
+# ----------
