4d9a137f46cceda01077ecfe67fcbbe8c8d5f00d
NiPeGun <nipegun@gmail.com>
2024-11-15 11:24:57 +0100

    Update Artefactos-Windows-Obtener-DeImagen.sh

--- Artefactos-Windows-Obtener-DeImagen.sh
+++ Artefactos-Windows-Obtener-DeImagen.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para analizar y exportar el archivo $MFT original a múltimples formatos
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Artefactos-Windows-Obtener-DeEvidencia-EnImagen.sh | sudo bash -s /Ruta/Al/Archivo/De/Imagen/De/Evidencia
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Artefactos-Windows-Obtener-DeImagen.sh | sudo bash -s /Ruta/Al/Archivo/De/Imagen/De/Evidencia
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Artefactos-Windows-Obtener-DeEvidencia-EnImagen.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Artefactos-Windows-Obtener-DeImagen.sh | nano -
 # ----------
 
 # Definir constantes de color
