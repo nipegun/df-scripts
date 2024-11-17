c8c66928adba5902944396f73edc79543b782a11
NiPeGun <nipegun@gmail.com>
2024-11-17 16:47:36 +0100

    Update and rename Windows-Artefactos-Extraer-DeImagen.sh to Windows-Artefactos-Todos-Extraer-DeImagen.sh

--- Windows-Artefactos-Todos-Extraer-DeImagen.sh
+++ Windows-Artefactos-Todos-Extraer-DeImagen.sh
@@ -9,10 +9,10 @@
 # Script de NiPeGun para analizar y exportar el archivo $MFT original a múltimples formatos
 #
 # Ejecución remota:
-#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Windows-Artefactos-Extraer-DeImagen.sh | sudo bash -s [RutaAlArchivoDeImagen]
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Windows-Artefactos-Todos-Extraer-DeImagen.sh | sudo bash -s [RutaAlArchivoDeImagen]
 #
 # Bajar y editar directamente el archivo en nano
-#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Windows-Artefactos-Extraer-DeImagen.sh | nano -
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Windows-Artefactos-Todos-Extraer-DeImagen.sh | nano -
 # ----------
 
 # Definir constantes de color
