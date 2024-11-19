8e4fc1eed390f192ddf43a5cce8d384adbde4d8b
NiPeGun <nipegun@gmail.com>
2024-11-19 12:55:45 +0100

    Update Windows-Artefactos-RAM-Extraer-DeDump6.sh

--- Windows-Artefactos-RAM-Extraer-DeDump6.sh
+++ Windows-Artefactos-RAM-Extraer-DeDump6.sh
@@ -25,36 +25,39 @@
   cFinColor='\033[0m'
 
 # Definir la cantidad de argumentos esperados
-  cCantParamEsperados=1
+  cCantParamEsperados=2
 
 if [ $# -ne $cCantParamEsperados ]
   then
     echo ""
     echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
-    echo "    $0 [RutaAlArchivoConDump]"
+    echo "    $0 [RutaAlArchivoConDump] [CarpetaDondeGuardar]"
     echo ""
     echo "  Ejemplo:"
-    echo "    $0 'Hola'"
+    echo "    $0 [/Casos/a2024m11d24/Dump/RAM.dump] [/Casos/a2024m11d24/Artefactos]"
     echo ""
     exit
   else
     # Crear constante para archivo de dump
-      cRutaAlArchivoDeDump="$2"
-      
+      cRutaAlArchivoDeDump="$1"
+      cCarpetaDondeGuardar="$2"
+      mkdir -p "$cCarpetaDondeGuardar"
+
+    # Entrar en el entorno virtual
+      python3 -m venv ~/PythonVirtualEnvironments/volatility3
       source ~/PythonVirtualEnvironments/volatility3/bin/activate
     # Información del sistema operativo
       echo ""
       echo "  Extrayendo información del sistema operativo..."
       echo ""
-      ~/PythonVirtualEnvironments/volatility3/bin/activate
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.info
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.info > "$cCarpetaDondeGuardar"/windows.info.txt
     # Procesos
       echo ""
       echo "  Extrayendo información de procesos"
       echo ""
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.pslist
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.psscan
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.pstree
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.pslist > "$cCarpetaDondeGuardar"/windows.pslist.txt
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.psscan > "$cCarpetaDondeGuardar"/windows.psscan.txt
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.pstree > "$cCarpetaDondeGuardar"/windows.pstree.txt
     # ProcDump (Dumpea .exes y DLLs asociadas)
       ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" -o "/path/to/dir" windows.dumpfiles ‑‑pid "<PID>" 
     # MemDump
@@ -67,26 +70,26 @@ if [ $# -ne $cCantParamEsperados ]
       ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.dlllist ‑‑pid "<PID>"
 
     # CMDLine (PID, process name, args)
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.cmdline
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.cmdline > "$cCarpetaDondeGuardar"/windows.cmdline.txt
 
     # Red
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.netscan
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.netstat
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.netscan > "$cCarpetaDondeGuardar"/windows.netscan.txt
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.netstat > "$cCarpetaDondeGuardar"/windows.netstat.txt
 
 # Registro
 
   # HiveList
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.hivescan
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.hivelist
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.hivescan > "$cCarpetaDondeGuardar"/windows.registry.hivescan.txt
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.hivelist > "$cCarpetaDondeGuardar"/windows.registry.hivelist.txt
 
   # Registry printkey
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.printkey
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.printkey > "$cCarpetaDondeGuardar"/windows.registry.printkey.txt
       ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.registry.printkey ‑‑key "Software\Microsoft\Windows\CurrentVersion"
 
 # Archivos
 
   # FileScan
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.filescan
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.filescan > "$cCarpetaDondeGuardar"/windows.filescan.txt
 
   # FileDump
       ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" -o "/path/to/dir" windows.dumpfiles
@@ -96,7 +99,7 @@ if [ $# -ne $cCantParamEsperados ]
 # Misceláneo
 
   # MalFind (Dumpea PID, process name, process start, protection, commit charge, privatememory, file output, hexdump disassembly)
-      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.malfind
+      ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.malfind > "$cCarpetaDondeGuardar"/windows.malfind.txt
 
   # Yarascan
       ~/scripts/python/volatility3/vol.py -f "$cRutaAlArchivoDeDump" windows.vadyarascan ‑‑yara-rules "<string>"
