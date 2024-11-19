55ad9adc99ec312eb144822153fcf589df7ced13
NiPeGun <nipegun@gmail.com>
2024-11-19 12:19:46 +0100

    Update Windows-Artefactos-RAM-Extraer-DeDump.sh

--- Windows-Artefactos-RAM-Extraer-DeDump.sh
+++ Windows-Artefactos-RAM-Extraer-DeDump.sh
@@ -5,6 +5,25 @@
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
 # No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
 
+# ----------
+# Script de NiPeGun para parsear datos extraidos de la RAM de Windows en Debian
+#
+# Ejecución remota con sudo:
+#   curl -sL x | sudo bash
+#
+# Ejecución remota como root:
+#   curl -sL x | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
 # Definir constantes de color
   cColorAzul="\033[0;34m"
   cColorAzulClaro="\033[1;34m"
