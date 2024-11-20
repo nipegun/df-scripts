4e9bf33fce71159a53411d6e2c6175f25d80604d
NiPeGun <nipegun@gmail.com>
2024-11-21 00:09:45 +0100

    Update Volatility-Instalar.sh

--- SoftInst/ParaCLI/Volatility-Instalar.sh
+++ SoftInst/ParaCLI/Volatility-Instalar.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+para el 3 (Hacer lo mismo que volatility 2 para debian 11)
+sudo apt install -y build-essential git libdistorm3-dev yara libraw1394-11 libcapstone-dev capstone-tool tzdata
+sudo apt install -y python3 python3-dev libpython3-dev python3-pip python3-setuptools python3-wheel
+python3 -m pip install -U distorm3 yara pycrypto pillow openpyxl ujson pytz ipython capstone
+python3 -m pip install -U git+https://github.com/volatilityfoundation/volatility3.git
+echo 'export PATH=/home/username/.local/bin:$PATH' >> ~/.bashrc
+
+
 # Pongo a disposición pública este script bajo el término de "software de dominio público".
 # Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
