4e65ce3ecdb5dd0eac5b584300fe2dd8bb3e410f
NiPeGun <nipegun@gmail.com>
2024-11-28 22:49:30 +0100

    Update Windows-Artefactos-Registro-Parsear-WindowsVistaYPosterior.sh

--- Windows-Artefactos-Registro-Parsear-WindowsVistaYPosterior.sh
+++ Windows-Artefactos-Registro-Parsear-WindowsVistaYPosterior.sh
@@ -101,6 +101,6 @@ if [ $# -ne $cCantParamEsperados ]
       done < "/tmp/CarpetasDeUsuarios.txt"
 
     # Reparar permisos
-      sudo chown 1000:1000 "$vCarpetaDondeGuardar" -R
+      sudo chown $USER:$USER "$vCarpetaDondeGuardar" -R
 
 fi
