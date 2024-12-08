a0a37b24cf4094a20eb8a5f5d4601a7a6d8a13a5
NiPeGun <nipegun@gmail.com>
2024-12-08 15:24:41 +0100

    Update Windows-Artefactos-Todos-Parsear.sh

--- Windows-Artefactos-Todos-Parsear.sh
+++ Windows-Artefactos-Todos-Parsear.sh
@@ -33,7 +33,8 @@
       echo ""
       echo -e "${cColorRojo}  El paquete dialog no está instalado. Iniciando su instalación...${cFinColor}"
       echo ""
-      sudo apt-get -y update && sudo apt-get -y install dialog
+      sudo apt-get -y update
+      sudo apt-get -y install dialog
       echo ""
     fi
   menu=(dialog --checklist "Marca las opciones que quieras instalar:" 22 72 16)
