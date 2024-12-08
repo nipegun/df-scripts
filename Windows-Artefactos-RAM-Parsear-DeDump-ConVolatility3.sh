950bbac7bdeca12c7fe6650e081b237a58ddf82e
NiPeGun <nipegun@gmail.com>
2024-12-08 16:05:05 +0100

    Update Windows-Artefactos-RAM-Parsear-DeDump-ConVolatility3.sh

--- Windows-Artefactos-RAM-Parsear-DeDump-ConVolatility3.sh
+++ Windows-Artefactos-RAM-Parsear-DeDump-ConVolatility3.sh
@@ -188,6 +188,20 @@
             for key in "${!aOffsetsArchivosDocumentos[@]}"; do
               vol --quiet -f "$cRutaAlArchivoDeDump" -o "$cCarpetaDondeGuardar"/Archivos/Individuales/Documentos/ windows.dumpfiles --virtaddr $key
             done
+          # Eliminar archivos con hash duplicado
+            find "$cCarpetaDondeGuardar"/Archivos/Individuales/Documentos/ -type f | while read -r vArchivo; do
+              # Calcular el hash del archivo (usa sha256sum o md5sum según prefieras)
+                vHash=$(sha256sum "$vArchivo" | awk '{print $1}')
+              # Verificar si el hash ya existe en el array
+                if [[ -v hashes["$vHash"] ]]; then
+                  # Si el hash ya existe, eliminar el archivo duplicado
+                  echo "Eliminando duplicado: $vArchivo (hash: $vHash)"
+                  rm -f "$vArchivo"
+                else
+                  # Si el hash no existe, guardar el archivo en el array
+                  hashes["$vHash"]="$vArchivo"
+                fi
+            done
 
         ;;
 
