6a609d7ac759d96be7afb3ec2b4fe4f5cba0d2de
NiPeGun <nipegun@gmail.com>
2024-12-08 18:05:35 +0100

    Update Windows-Artefactos-RAM-Extraer-DeDump-ConVolatility2.sh

--- Windows-Artefactos-RAM-Extraer-DeDump-ConVolatility2.sh
+++ Windows-Artefactos-RAM-Extraer-DeDump-ConVolatility2.sh
@@ -13,6 +13,8 @@
 #
 # Bajar y editar directamente el archivo en nano
 #   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Windows-Artefactos-RAM-Extraer-DeDump-ConVolatility2.sh | nano -
+#
+# Más info aquí: https://github.com/volatilityfoundation/volatility/wiki/Command-Reference
 # ----------
 
 # Definir constantes de color
