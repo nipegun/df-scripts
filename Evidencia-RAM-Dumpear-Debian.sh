9d74eaffd215a0784181a2c778314b594f300cf5
NiPeGun <nipegun@gmail.com>
2025-02-25 22:12:00 +0100

    Update and rename Evidencia-RAM-Dumpear-Debian4.sh to Evidencia-RAM-Dumpear-Debian.sh

--- Evidencia-RAM-Dumpear-Debian.sh
+++ Evidencia-RAM-Dumpear-Debian.sh
@@ -128,8 +128,8 @@
              echo "    Creando el archivo .json con los símbolos del kernel"
              echo ""
              vVersKernel="$(uname -r)"
-             /tmp/dwarf2json linux --elf "/usr/lib/debug/boot/vmlinux-$vVersKernel" > "/tmp/Debian_linux-image-$vVersKernel.json"
-             /tmp/dwarf2json linux --elf "/usr/lib/debug/boot/vmlinux-$vVersKernel" --system-map "/usr/lib/debug/boot/System.map-$vVersKernel" > "/tmp/Debian_linux-image-$vVersKernel-SystemMap.json"
+             /tmp/dwarf2json linux --elf "/usr/lib/debug/boot/vmlinux-$vVersKernel" > "/tmp/Debian_$vVersKernel.json"
+             /tmp/dwarf2json linux --elf "/usr/lib/debug/boot/vmlinux-$vVersKernel" --system-map "/usr/lib/debug/boot/System.map-$vVersKernel" > "/tmp/Debian_$vVersKernel-SystemMap.json"
 
           ;;
 
