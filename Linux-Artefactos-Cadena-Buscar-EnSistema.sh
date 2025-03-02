8c861df2c1f0cfb91f6aa7811f545e8a7937e645
NiPeGun <nipegun@gmail.com>
2025-03-02 03:39:36 +0100

    Update Linux-Artefactos-Cadena-Buscar-EnSistema.sh

--- Linux-Artefactos-Cadena-Buscar-EnSistema.sh
+++ Linux-Artefactos-Cadena-Buscar-EnSistema.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para buscar y mostrar cadenas imprimibles específicas en todos los archivos del sistema Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul='\033[0;34m'
+  cColorAzulClaro='\033[1;34m'
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
 find / -type f -exec sh -c 'strings "$1" | grep -q "Uk17" && echo "Coincidencia de Uk17 en: $1"' _ {} \; 2>/dev/null
 echo ""
 find / -type f -exec sh -c 'strings "$1" | grep -q "RM{" && echo "Coincidencia de RM{ en: $1"' _ {} \; 2>/dev/null
