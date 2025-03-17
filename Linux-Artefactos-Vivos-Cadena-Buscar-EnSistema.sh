8fdbdcf40426207078dbe89a12eb97838afe6185
NiPeGun <nipegun@gmail.com>
2025-03-17 23:18:48 +0100

    Update Linux-Artefactos-Vivos-Cadena-Buscar-EnSistema.sh

--- Linux-Artefactos-Vivos-Cadena-Buscar-EnSistema.sh
+++ Linux-Artefactos-Vivos-Cadena-Buscar-EnSistema.sh
@@ -27,6 +27,8 @@
     #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
   cFinColor='\033[0m'
 
+#RM\{[^\}]+\}
+
 find / -type f -exec sh -c 'strings "$1" | grep -q "RM{" && echo "Coincidencia de RM{ en: $1"' _ {} \; 2>/dev/null
 echo ""
 find / -type f -exec sh -c 'strings "$1" | grep -q "Uk1" && echo "Coincidencia de Uk1 en: $1"' _ {} \; 2>/dev/null
