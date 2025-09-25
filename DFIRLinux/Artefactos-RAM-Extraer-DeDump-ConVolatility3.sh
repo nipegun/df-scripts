7b00dcc36d316ca9e5b567c1f871a3494e8f0482
NiPeGun <nipegun@gmail.com>
2025-09-25 05:04:20 +0200

    Update Artefactos-RAM-Extraer-DeDump-ConVolatility3.sh

--- DFIRLinux/Artefactos-RAM-Extraer-DeDump-ConVolatility3.sh
+++ DFIRLinux/Artefactos-RAM-Extraer-DeDump-ConVolatility3.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+
+#
+#
+#   TERMINAR LA PARTE FINAL DEL SCRIPT Y SACAR LAS COSAS DE WINDOWS
+#
+# PARTE FUNCIONAL (por ahora)
+#   Del 1 al 11
+#   Del 18 al 23
+#
+
+
+
+
 # Pongo a disposición pública este script bajo el término de "software de dominio público".
 # Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
 # Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
