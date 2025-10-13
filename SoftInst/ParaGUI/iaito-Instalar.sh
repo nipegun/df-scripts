e804decc5c00b7a28e558756b5c63b51f2ca2b0a
NiPeGun <nipegun@gmail.com>
2025-10-14 00:47:34 +0200

    Update iaito-Instalar.sh

--- SoftInst/ParaGUI/iaito-Instalar.sh
+++ SoftInst/ParaGUI/iaito-Instalar.sh
@@ -55,9 +55,63 @@
     echo -e "${cColorAzulClaro}  Iniciando el script de instalación de iaito para Debian 13 (x)...${cFinColor}"
     echo ""
 
-    echo ""
-    echo -e "${cColorRojo}    Comandos para Debian 13 todavía no preparados. Prueba ejecutarlo en otra versión de Debian.${cFinColor}"
-    echo ""
+    # Instalar primero radare2
+    
+      # Obtener el número de la última versión
+        # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+          if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+            echo ""
+            echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+            echo ""
+            sudo apt-get -y update
+            sudo apt-get -y install curl
+            echo ""
+          fi
+        # Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
+          if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+            echo ""
+            echo -e "${cColorRojo}  El paquete jq no está instalado. Iniciando su instalación...${cFinColor}"
+            echo ""
+            sudo apt-get -y update
+            sudo apt-get -y install jq
+            echo ""
+          fi
+        vUltVersR2=$(curl -s https://api.github.com/repos/radareorg/radare2/releases/latest | jq -r .tag_name)
+
+      # Descargar el paquete
+        curl -L https://github.com/radareorg/radare2/releases/download/"$vUltVersR2"/radare2_"$vUltVersR2"_amd64.deb -o /tmp/radare2.deb
+
+      # Instalar
+        sudo apt -y install /tmp/radare2.deb
+
+    # Finalmente, instalar iaito
+    
+      # Obtener el número de la última versión
+        # Comprobar si el paquete curl está instalado. Si no lo está, instalarlo.
+          if [[ $(dpkg-query -s curl 2>/dev/null | grep installed) == "" ]]; then
+            echo ""
+            echo -e "${cColorRojo}  El paquete curl no está instalado. Iniciando su instalación...${cFinColor}"
+            echo ""
+            sudo apt-get -y update
+            sudo apt-get -y install curl
+            echo ""
+          fi
+        # Comprobar si el paquete jq está instalado. Si no lo está, instalarlo.
+          if [[ $(dpkg-query -s jq 2>/dev/null | grep installed) == "" ]]; then
+            echo ""
+            echo -e "${cColorRojo}  El paquete jq no está instalado. Iniciando su instalación...${cFinColor}"
+            echo ""
+            sudo apt-get -y update
+            sudo apt-get -y install jq
+            echo ""
+          fi
+        vUltVersIAITO=$(curl -s https://api.github.com/repos/radareorg/iaito/releases/latest | jq -r .tag_name)
+
+      # Descargar el paquete
+        curl -L https://github.com/radareorg/iaito/releases/download/"$vUltVersIAITO"/iaito_"$vUltVersIAITO"_amd64.deb -o /tmp/iaito.deb
+
+      # Instalar
+        sudo apt -y install /tmp/iaito.deb
 
   elif [ $cVerSO == "12" ]; then
 
@@ -89,7 +143,7 @@
         vUltVersR2=$(curl -s https://api.github.com/repos/radareorg/radare2/releases/latest | jq -r .tag_name)
 
       # Descargar el paquete
-        curl -L https://github.com/radareorg/radare2/releases/download/"$vUltVers"/radare2_"$vUltVers"_amd64.deb -o /tmp/radare2.deb
+        curl -L https://github.com/radareorg/radare2/releases/download/"$vUltVersR2"/radare2_"$vUltVersR2"_amd64.deb -o /tmp/radare2.deb
 
       # Instalar
         sudo apt -y install /tmp/radare2.deb
