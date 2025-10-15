42189309ba9e1060484ff1d8db102c0b68aed3b7
NiPeGun <nipegun@gmail.com>
2025-10-15 17:48:27 +0200

    Update Rizin-Instalar.sh

--- SoftInst/ParaCLI/Rizin-Instalar.sh
+++ SoftInst/ParaCLI/Rizin-Instalar.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para instalar Rizin en Debian
+#
+# Ejecución remota:
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/SoftInst/ParaCLI/Rizin-Instalar.sh | bash
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/SoftInst/ParaCLI/Rizin-Instalar.sh | nano -
+# ----------
+
 # Actualizar lista de paquetes disponibles en los repositorios
   sudo apt-get -y update
 
