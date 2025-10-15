6d75ba20f9914b268cf764b1307a381df37891dc
NiPeGun <nipegun@gmail.com>
2025-10-15 21:05:20 +0200

    Update Decompilar.sh

--- Reversing/Binarios/Decompilar.sh
+++ Reversing/Binarios/Decompilar.sh
@@ -83,16 +83,16 @@
     echo ""
     echo "      Decompilando funciones..."
     echo ""
-    r2 -e bin.relocs.apply=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
+    r2 -e bin.relocs.apply=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
     rz-bin -v
     echo ""
-    r2 -AA -e bin.relocs.apply=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
+    r2 -AA -e bin.relocs.apply=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
     echo ""
     echo "      Decompilando completamente..."
     echo ""
-    r2 -e bin.relocs.apply=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
+    r2 -e bin.relocs.apply=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
     echo ""
-    r2 -AA -e bin.relocs.apply=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
+    r2 -AA -e bin.relocs.apply=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario" > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
     echo ""
   elif file "$cRutaAbsolutaAlArchivoBinario" | grep -q "PE32"; then   # (Para .exe o .dll)
     echo ""
@@ -103,15 +103,15 @@
     echo ""
     echo "      Decompilando funciones..."
     echo ""
-    r2 -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
+    r2 -e bin.cache=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
     echo ""
-    r2 -AA -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
+    r2 -AA -e bin.cache=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
     echo ""
     echo "      Decompilando completamente..."
     echo ""
-    r2 -e bin.cache=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
+    r2 -e bin.cache=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
     echo ""
-    r2 -AA -e bin.cache=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
+    r2 -AA -e bin.cache=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
     echo ""
   elif file "$cRutaAbsolutaAlArchivoBinario" | grep -q "Mach-O"; then # (Para macOS)
     echo ""
@@ -122,15 +122,15 @@
     echo ""
     echo "      Decompilando funciones..."
     echo ""
-    r2 -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
+    r2 -e bin.cache=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones.txt
     echo ""
-    r2 -AA -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
+    r2 -AA -e bin.cache=true -Aqc "e scr.color=0; afl" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-ListaDeFunciones-Experimental.txt
     echo ""
     echo "      Decompilando completamente..."
     echo ""
-    r2 -e bin.cache=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
+    r2 -e bin.cache=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado.c
     echo ""
-    r2 -AA -e bin.cache=true -Aqc "pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
+    r2 -AA -e bin.cache=true -Aqc "e scr.color=0; pdd" "$cRutaAbsolutaAlArchivoBinario"        > "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-Decompilado-Experimental.c
     echo ""
   else
     echo "No se pudo determinar si el binario es de Linux, Windows o macOS"
