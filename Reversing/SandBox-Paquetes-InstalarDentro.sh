77871a99a98bd489136913c3a2ad955c586b1a31
NiPeGun <nipegun@gmail.com>
2025-10-15 23:35:49 +0200

    Create SandBox-Paquetes-InstalarDentro.sh

---
+++ Reversing/SandBox-Paquetes-InstalarDentro.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ejecuciópn remota:
+#  curl -sL 
+
+apt-get -y update
+apt-get -y install strace
+apt-get -y install libgl1
+apt-get -y install libxrandr2
+apt-get -y install libxi6
+apt-get -y install libxcursor1
+apt-get -y install libxinerama1
+apt-get -y install binutils # Para el comando strings
+apt-get -y install gdb
+apt-get -y install xxd
+apt-get -y install bzip2
+apt-get -y install file
+apt-get -y install locales
+sed -i 's/^# *en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^# *es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
