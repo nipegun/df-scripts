a9cce0a1d1f4b5456029d3d20aa3f2c032e8da85
NiPeGun <nipegun@gmail.com>
2025-10-15 22:44:40 +0200

    Add installation of binutils for strings command

--- Reversing/SandBox-Crear.sh
+++ Reversing/SandBox-Crear.sh
@@ -45,6 +45,7 @@ vNombreContenedor="sandbox-debian"
     apt-get -y install libxi6
     apt-get -y install libxcursor1
     apt-get -y install libxinerama1
+    apt-get -y install binutils # Para el comando strings
     apt-get -y install gdb
     apt-get -y install locales
     sed -i 's/^# *es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
