ab76a49f89b2b4f3884756c195cf707e1051cb31
NiPeGun <nipegun@gmail.com>
2025-10-15 16:53:46 +0200

    Create Decompilar.sh

---
+++ Reversing/Binarios/Decompilar.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para decompilar binarios en Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Reversing/Binarios/Decompilar.sh | bash -s [RutaAlArchivoBinario]
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Reversing/Binarios/Decompilar.sh | sed 's-sudo--g' | bash -s [RutaAbsolutaAlArchivoBinario]
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Reversing/Binarios/Decompilar.sh | nano -
+# ----------
+
+# Definir constantes de color
+  cColorAzul="\033[0;34m"
+  cColorAzulClaro="\033[1;34m"
+  cColorVerde='\033[1;32m'
+  cColorRojo='\033[1;31m'
+  # Para el color rojo también:
+    #echo "$(tput setaf 1)Mensaje en color rojo. $(tput sgr 0)"
+  cFinColor='\033[0m'
+
+# Definir la cantidad de argumentos esperados
+  cCantParamEsperados=1
+
+# Comprobar que se hayan pasado la cantidad de parámetros correctos. Abortar el script si no.
+  if [ $# -ne $cCantParamEsperados ]
+    then
+      echo ""
+      echo -e "${cColorRojo}  Mal uso del script. El uso correcto sería: ${cFinColor}"
+      echo "    $0 [RutaAbsolutaAlArchivoBinario]"
+      echo ""
+      echo "  Ejemplo:"
+      echo "    $0 '/tmp/binario.bin'"
+      echo ""
+      exit
+  fi
+
+# Comprobar que exista el archivo
+  if [ ! -f "$1" ]; then
+    echo ""
+    echo -e "${cColorRojo}    El archivo pasado como parámetro no existe. Abortando... ${cFinColor}"
+    echo ""
+    exit
+  fi
+
+# Definir constantes
+  cRutaAbsolutaAlArchivoBinario="$1"
+  cCarpetaDelArchivoBinario="$(echo "$cRutaAbsolutaAlArchivoBinario" | rev | cut -d'/' -f2- | rev)"
+  cCarpetaDondeGuardar="$(echo "$cCarpetaDelArchivoBinario""/InfoDelBinario/")"
+  cNombreDeArchivo="$(basename "$cRutaAbsolutaAlArchivoBinario")"
+
+# Crear carpeta
+  sudo mkdir -p "$cCarpetaDondeGuardar"/Decompilado/
+  sudo chown $USER:$USER "$cCarpetaDondeGuardar" -R
+
+# Obtener info del elf
+
+  # file
+    # Comprobar si el paquete file está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s file 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${cColorRojo}    El paquete file no está instalado. Iniciando su instalación...${cFinColor}"
+        echo ""
+        sudo apt-get -y update
+        sudo apt-get -y install file
+        echo ""
+      fi 
+    file "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".file.txt
+
+  # readelf
+    # Comprobar si el paquete binutils está instalado. Si no lo está, instalarlo.
+      if [[ $(dpkg-query -s binutils 2>/dev/null | grep installed) == "" ]]; then
+        echo ""
+        echo -e "${cColorRojo}    El paquete binutils no está instalado. Iniciando su instalación...${cFinColor}"
+        echo ""
+        sudo apt-get -y update
+        sudo apt-get -y install binutils
+        echo ""
+      fi
+
+# Determinar si es un binario de Linux, de macOS o de Windows
+  if file "$cRutaAbsolutaAlArchivoBinario"   | grep -q "ELF"; then    # (Para Linux)
+    echo ""
+    echo "    El archivo parece ser un binario de Linux."
+    echo ""
+    echo "      Decompilando funciones..."
+    echo ""
+    r2 -e bin.relocs.apply=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-funciones.txt
+    echo ""
+  elif file "$cRutaAbsolutaAlArchivoBinario" | grep -q "PE32"; then   # (Para .exe o .dll)
+    echo ""
+    echo "    El archivo parece ser un binario de Windows."
+    echo ""
+    echo "      Decompilando funciones..."
+    echo ""
+    r2 -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-funciones.txt
+    echo ""
+  elif file "$cRutaAbsolutaAlArchivoBinario" | grep -q "Mach-O"; then # (Para macOS)
+    echo ""
+    echo "    El archivo parece ser un binario de macOS."
+    echo ""
+    echo "      Decompilando funciones..."
+    echo ""
+    r2 -e bin.cache=true -Aqc "afl" "$cRutaAbsolutaAlArchivoBinario"        &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".r2-funciones.txt
+    echo ""
+  else
+    echo "No se pudo determinar si el binario es de Linux, Windows o macOS"
+  fi
+
+# Reparar permisos
+  sudo chown $USER:$USER "$cCarpetaDondeGuardar" -R
+
+# Notificar fin de ejecución del script
+  echo ""
+  echo "  Ejecución del script, finalizada..."
+  echo ""
+
