f52973b676660dec0fb87b2405cb23ce0c455f26
NiPeGun <nipegun@gmail.com>
2025-10-17 12:54:25 +0200

    Update and rename 01-SandBox-Paquetes-InstalarDentro.sh to 01-InSandBox-Entorno-Preparar.sh

--- Reversing/01-InSandBox-Entorno-Preparar.sh
+++ Reversing/01-InSandBox-Entorno-Preparar.sh
@@ -3,6 +3,13 @@
 # Ejecución remota únicamente como root:
 #  curl -sL https://raw.githubusercontent.com/nipegun/df-scripts/refs/heads/main/Reversing/01-SandBox-Paquetes-InstalarDentro.sh | bash
 
+echo 'SanboxParaReversing'            | tee -a /etc/hostname
+echo '127.0.0.1 SandboxParaReversing' | tee -a /etc/hosts
+echo '127.0.1.1 SandboxParaReversing' | tee -a /etc/hosts
+
+mkdir /mnt/host/tmp
+chmod 777 /mnt/host/tmp/
+
 apt-get -y update
 apt-get -y install sudo
 apt-get -y install strace
@@ -33,3 +40,7 @@ echo 'export LC_ALL=en_US.UTF-8'   >> /etc/profile
 echo 'export LANGUAGE=en_US.UTF-8' >> /etc/profile
 locale-gen
 update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8
+
+# Entrar en la carpeta montada del host
+  cd /mnt/host/
+
