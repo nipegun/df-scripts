93cdc9c56cf2f85231277ee321c49f32081fac76
NiPeGun <nipegun@gmail.com>
2025-10-19 23:28:05 +0200

    Add 00-ObtenerInfo3.sh file

--- Reversing/Binarios/00-ObtenerInfo3.sh
+++ Reversing/Binarios/00-ObtenerInfo3.sh
@@ -88,8 +88,8 @@
     readelf -h -l -S -s -r -d -V -A -I -n -N -x . -w -W          "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".readelf.txt
 
   # objdump
-    #objdump -s -S -D -r -R -t -T -p -f -g -G -h -w -W --source "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".objdump.txt
-    objdump -d --source "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".objdump.txt
+    objdump -s -S -D -r -R -t -T -p -f -g -G -h -w -W --source "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".objdump.txt
+    #objdump -D --source "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".objdump.txt
 
   # strings
     strings -a                                                   "$cRutaAbsolutaAlArchivoBinario" &> "$cCarpetaDondeGuardar""$cNombreDeArchivo".strings.txt
