da5b81eeb13de5306a4ad7646fddf27d6a6969c1
NiPeGun <nipegun@gmail.com>
2025-12-11 21:58:36 +0100

    Create EmpaquetadoPorPyInstaller-Desempaquetar.sh

---
+++ Reversing/Binarios/EmpaquetadoPorPyInstaller-Desempaquetar.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+# ----------
+# Script de NiPeGun para desempaquetar un python empaquetado con PyInstaller usando Debian
+#
+# Ejecución remota (puede requerir permisos sudo):
+#   curl -sL x | bash
+#
+# Ejecución remota como root (para sistemas sin sudo):
+#   curl -sL x | sed 's-sudo--g' | bash
+#
+# Ejecución remota sin caché:
+#   curl -sL -H 'Cache-Control: no-cache, no-store' x | bash
+#
+# Ejecución remota con parámetros:
+#   curl -sL x | bash -s Parámetro1 Parámetro2
+#
+# Bajar y editar directamente el archivo en nano
+#   curl -sL x | nano -
+# ----------
+
+cArchivoBinario="$1"
+
+# Bajar el script de extracción
+  cp "$cArchivoBinario" /tmp/Binario
+  wget https://raw.githubusercontent.com/extremecoders-re/pyinstxtractor/master/pyinstxtractor.py
+
+# Extraer
+  python3 /tmp/pyinstxtractor.py /tmp/Binario
+
