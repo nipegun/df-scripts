88bddaa68b813af645206bd6f8d091e10d6965a5
NiPeGun <nipegun@gmail.com>
2025-12-11 22:11:27 +0100

    Create Python-PuntoPYC-Decompilar-TodosEnCarpeta.sh

---
+++ Reversing/Binarios/Python-PuntoPYC-Decompilar-TodosEnCarpeta.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# sudo python3 -m pip install decompyle3 --break-system-packages
+
+# decompyle3 Binario_extracted/malware.pyc > malware.py
+
+for f in *.pyc; do decompyle3 "$f" > "${f%.pyc}.py"; done
+
